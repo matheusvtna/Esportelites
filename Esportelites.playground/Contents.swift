f612dd9543889a60d04d511bf093dcbc082982e5
Matheus Vinícius Teotonio do Nascmento Andrade <matheus@MacBook-Pro-de-suporte.local>
2020-03-31 18:06:36 -0300

    Tela de quiz com labe e imagem. falta retirar botao de toque

--- Esportelites.playground/Contents.swift
+++ Esportelites.playground/Contents.swift
@@ -7,24 +7,34 @@ class MyViewController : UIViewController {
     
     let buttonChangeColor = UIButton()
     let backgroundQuiz = UIImageView()
-    let label = UILabel()
+    let sportLabel = UILabel()
+    let sportImage = UIImageView()
 
     let fontLabel = UIFont(name: "ChalkboardSE-Light", size: 96)
     let fontButton = UIFont(name: "ChalkboardSE-Light", size: 36)
+    
     override func loadView() {
         let view = UIView()
 
+        // Background
         backgroundQuiz.contentMode = .scaleToFill
         backgroundQuiz.frame = CGRect(x: 0, y: 0, width: 1440, height: 900)
         backgroundQuiz.image = UIImage(imageLiteralResourceName: "Background.png")
         
-        label.frame = CGRect(x: 100, y: 60, width: 1230, height: 100)
-        label.textAlignment = .center
-        label.text = "Baseball"
-        label.font = fontLabel
-        label.textColor = #colorLiteral(red: 0.8823529412, green: 0.6470588235, blue: 0.2745098039, alpha: 1)
+        // Sport Label
+        sportLabel.frame = CGRect(x: 90, y: 10, width: 1210, height: 100)
+        sportLabel.textAlignment = .center
+        sportLabel.text = "Baseball"
+        sportLabel.font = fontLabel
+        sportLabel.textColor = #colorLiteral(red: 0.8823529412, green: 0.6470588235, blue: 0.2745098039, alpha: 1)
 
-        buttonChangeColor.frame = CGRect(x: 601, y: 503, width: 219, height: 67)
+        // Sport Image
+        sportImage.contentMode = .scaleToFill
+        sportImage.frame = CGRect(x: 650, y: 125, width: 110, height: 110)
+        sportImage.image = UIImage(imageLiteralResourceName: "BaseballImage.png")
+        
+        // Next Question Button
+        buttonChangeColor.frame = CGRect(x: 591, y: 503, width: 219, height: 67)
         buttonChangeColor.setTitle("Toque", for: .normal)
         buttonChangeColor.titleLabel?.font = fontButton
         buttonChangeColor.titleLabel?.textAlignment = .center
@@ -33,7 +43,9 @@ class MyViewController : UIViewController {
         buttonChangeColor.addTarget(self, action: #selector(touchedButton), for: .touchUpInside)
         
         view.addSubview(backgroundQuiz)
-        view.addSubview(label)
+        view.addSubview(sportLabel)
+        
+        view.addSubview(sportImage)
         view.addSubview(buttonChangeColor)
                 
         self.view = view
