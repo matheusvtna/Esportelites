3474d438440d1ca7cc2cac6922b26e6f9d25c42b
Matheus Vinícius Teotonio do Nascmento Andrade <matheus@MacBook-Pro-de-suporte.local>
2020-03-31 17:49:36 -0300

    Reorganizando label e botao com nova fonte

--- Esportelites.playground/Contents.swift
+++ Esportelites.playground/Contents.swift
@@ -6,27 +6,36 @@ import PlaygroundSupport
 class MyViewController : UIViewController {
     
     let buttonChangeColor = UIButton()
-    
+    let backgroundQuiz = UIImageView()
+    let label = UILabel()
+
+    let fontLabel = UIFont(name: "ChalkboardSE-Light", size: 96)
+    let fontButton = UIFont(name: "ChalkboardSE-Light", size: 36)
     override func loadView() {
         let view = UIView()
-        
-        let backgroundQuiz = UIImageView()
+
         backgroundQuiz.contentMode = .scaleToFill
         backgroundQuiz.frame = CGRect(x: 0, y: 0, width: 1440, height: 900)
         backgroundQuiz.image = UIImage(imageLiteralResourceName: "Background.png")
         
-        let label = UILabel()
-        label.frame = CGRect(x: 105, y: 56, width: 1229, height: 92)
+        label.frame = CGRect(x: 100, y: 60, width: 1230, height: 100)
         label.textAlignment = .center
         label.text = "Baseball"
-        label.font = UIFont(name: "Marker Felt Amplo", size: 72)
+        label.font = fontLabel
         label.textColor = #colorLiteral(red: 0.8823529412, green: 0.6470588235, blue: 0.2745098039, alpha: 1)
 
-        view.addSubview(backgroundQuiz)
-        view.addSubview(label)
-        
+        buttonChangeColor.frame = CGRect(x: 601, y: 503, width: 219, height: 67)
+        buttonChangeColor.setTitle("Toque", for: .normal)
+        buttonChangeColor.titleLabel?.font = fontButton
+        buttonChangeColor.titleLabel?.textAlignment = .center
+        buttonChangeColor.setTitleColor(.black, for: .normal)
+        buttonChangeColor.setBackgroundImage(UIImage(imageLiteralResourceName: "proxPergunta.png"), for: .normal)
         buttonChangeColor.addTarget(self, action: #selector(touchedButton), for: .touchUpInside)
         
+        view.addSubview(backgroundQuiz)
+        view.addSubview(label)
+        view.addSubview(buttonChangeColor)
+                
         self.view = view
     }
     
@@ -46,7 +55,6 @@ class MyViewController : UIViewController {
 //        for fonte in fontNames{
 //            print(fonte)
 //        }
-//
 
 let mvc = MyViewController(screenType: .mac, isPortrait: true)
 
