ead9b624cee897fe7632109b4c9ed904d7ed4daf
Matheus Vinícius Teotonio do Nascmento Andrade <matheus@MacBook-Pro-de-suporte.local>
2020-03-31 16:56:11 -0300

    Adicionando background e labels

--- Esportelites.playground/Contents.swift
+++ Esportelites.playground/Contents.swift
@@ -4,18 +4,50 @@ import UIKit
 import PlaygroundSupport
 
 class MyViewController : UIViewController {
+    
+    let buttonChangeColor = UIButton()
+    
     override func loadView() {
         let view = UIView()
-        view.backgroundColor = .white
-
-        let label = UILabel()
-        label.frame = CGRect(x: 150, y: 200, width: 200, height: 20)
-        label.text = "Esportélites!"
-        label.textColor = .black
         
+        let backgroundQuiz = UIImageView()
+        backgroundQuiz.contentMode = .scaleToFill
+        backgroundQuiz.frame = CGRect(x: 0, y: 0, width: 1440, height: 900)
+        backgroundQuiz.image = UIImage(imageLiteralResourceName: "Background.png")
+        
+        let label = UILabel()
+        label.frame = CGRect(x: 105, y: 56, width: 1229, height: 92)
+        label.textAlignment = .center
+        label.text = "Baseball"
+        label.font = UIFont(name: "Marker Felt Amplo", size: 72)
+        label.textColor = #colorLiteral(red: 0.8823529412, green: 0.6470588235, blue: 0.2745098039, alpha: 1)
+
+        view.addSubview(backgroundQuiz)
         view.addSubview(label)
+        
+        buttonChangeColor.addTarget(self, action: #selector(touchedButton), for: .touchUpInside)
+        
         self.view = view
     }
+    
+    @IBAction func touchedButton(){
+        print("Oi")
+    }
+    
 }
-// Present the view controller in the Live View window
-PlaygroundPage.current.liveView = MyViewController()
+
+//        var fontNames = [String]()
+//
+//        for name in UIFont.familyNames {
+//            fontNames.append(contentsOf: UIFont.fontNames(forFamilyName: name) as [String])
+//        }
+//
+//        fontNames.sorted()
+//        for fonte in fontNames{
+//            print(fonte)
+//        }
+//
+
+let mvc = MyViewController(screenType: .mac, isPortrait: true)
+
+PlaygroundPage.current.liveView = mvc.scale(to: 0.4)
