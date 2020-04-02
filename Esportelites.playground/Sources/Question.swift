6b47414ee5fe612b5abad4c4fbf2c7a78bcbf7bf
Matheus Vinícius Teotonio do Nascmento Andrade <matheus@MacBook-Pro-de-suporte.local>
2020-04-02 16:41:56 -0300

    Nova classe definindo o quiz

--- Esportelites.playground/Sources/Question.swift
+++ Esportelites.playground/Sources/Question.swift
@@ -7,15 +7,31 @@ public class Question{
     public let alternativeB: String
     public let alternativeC: String
     public let alternativeD: String
+    public let answer: String
     public let information: String
     
-    public init(asking: String, labelA: String, labelB: String, labelC: String, labelD: String, inform: String){
+    public init(asking: String, labelA: String, labelB: String, labelC: String, labelD: String, answer: String, inform: String){
         self.asking = asking
         self.alternativeA = labelA
         self.alternativeB = labelB
         self.alternativeC = labelC
         self.alternativeD = labelD
+        self.answer = answer
         self.information = inform
+    }
+    
+}
+
+public class QuestionBank{
+    
+    public var quiz: [Question]
+    
+    public init() {
+        self.quiz = []
+    }
+    
+    public func addQuestion(question: Question){
+        self.quiz.append(question)
     }
     
 }
